d12ac1bf574a8f2c0e084dabf9e5f18d5d8b530d
s4tker <s4tker@gmail.com>
2026-04-15 17:08:19 +0000

    Actualización

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <title>ElectroShop | Tecnología y Más</title>
+    <title>ElectroShop</title>
 
     <script src="https://cdn.tailwindcss.com"></script>
     <meta name="csrf-token" content="{{ csrf_token() }}">
